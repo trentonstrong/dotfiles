9c47266927f7b3466c9bed91eb2ab136e6b7feb3
Trenton Strong <trent.strong@shopify.com>
2023-02-05 11:39:40 -0800

    add cli utils

--- personal/antigen_bundles.zsh
+++ personal/antigen_bundles.zsh
@@ -1,7 +1,7 @@
 # Additional oh-my-zsh plugins to include
 # Default bundles included can be seen in core/default_bundles.zsh
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Plugins for available oh-my-zsh plugins.
-# See https://github.com/zsh-users/antigen/wiki/Commands#antigen-bundle for instructions on including custom plugins. 
+# See https://github.com/zsh-users/antigen/wiki/Commands#antigen-bundle for instructions on including custom plugins.
 #
 # Include a plugin with `antigen bundle <plugin-name>`.
 
@@ -14,3 +14,5 @@
 
 # Staples theme: https://github.com/dersam/staples
 antigen theme dersam/staples staples
+
+antigen bundle fd
--- personal/install.sh
+++ personal/install.sh
@@ -1,2 +1,22 @@
 # Custom actions to take on initial install of dotfiles.
 # This runs after default install actions, so you can overwrite changes it makes if you want.
+
+ZSH_HOST_OS=$(uname | awk '{print tolower($0)}')
+
+case $ZSH_HOST_OS in
+  darwin*)
+
+  BREW_EXECUTABLE=/opt/homebrew/bin/brew
+
+  $BREW_EXECUTABLE install ripgrep
+  $BREW_EXECUTABLE install fd
+  $BREW_EXECUTABLE install bat
+  $BREW_EXECUTABLE install exa
+;;
+esac
+
+if [ $SPIN ]; then
+  sudo apt-get install -y fd
+  sudo apt-get install -y bat
+  sudo apt-get install -y exa
+fi
