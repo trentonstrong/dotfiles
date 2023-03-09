84ed112bf94a282631c3fc301d836addb4f761d1
Trenton Strong <trent.strong@shopify.com>
2023-03-09 13:57:54 -0500

    update personal tools

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,8 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+alias ls="exa"
+alias ll="ls -lh"
+alias docker="podman"
+alias docker-compose="podman-compose"
--- personal/install.sh
+++ personal/install.sh
@@ -12,11 +12,13 @@ case $ZSH_HOST_OS in
   $BREW_EXECUTABLE install fd
   $BREW_EXECUTABLE install bat
   $BREW_EXECUTABLE install exa
+  $BREW_EXECUTABLE install tig
 ;;
-esac
 
-if [ $SPIN ]; then
+  linux*)
+  sudo apt-get install -y ripgrep
   sudo apt-get install -y fd
   sudo apt-get install -y bat
   sudo apt-get install -y exa
-fi
+  sudo apt-get install -y tig
+esac
